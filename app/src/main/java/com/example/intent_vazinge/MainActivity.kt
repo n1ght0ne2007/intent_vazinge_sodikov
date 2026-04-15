9b72e0f0cbedd82b8aa5080dc61b273d83dce295
n1ght0ne2007 <sodikovzena1@gmail.com>
2026-04-15 14:35:54 +0300

    Add button

--- app/src/main/java/com/example/intent_vazinge/MainActivity.kt
+++ app/src/main/java/com/example/intent_vazinge/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.intent_vazinge
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Intent_vazingeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
@@ -32,11 +37,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+    Column {
+        Button(onClick = {callPhoneNumber()}) {
+            Text("Позвонить")
+        }
+        Button(onClick = {}) {
+            Text("Написать email")
+        }
+        Button(onClick = {}) {
+            Text("Показать офис на карте")
+        }
+        Button(onClick = {}) {
+            Text("Поделиться контактом")
+        }
+    }
 }
+fun callPhoneNumber(){
+    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+7912347"))
+}
+
 
 @Preview(showBackground = true)
 @Composable
