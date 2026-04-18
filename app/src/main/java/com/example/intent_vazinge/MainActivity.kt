028f17b2732c1b834da51e39a3791613dde7edb9
n1ght0ne2007 <sodikovzena1@gmail.com>
2026-04-18 16:49:33 +0300

    Готовый проект

--- app/src/main/java/com/example/intent_vazinge/MainActivity.kt
+++ app/src/main/java/com/example/intent_vazinge/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.intent_vazinge
 
+
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.intent_vazinge.ui.theme.Intent_vazingeTheme
 
@@ -24,44 +26,70 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Intent_vazingeTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Greeting()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Column {
-        Button(onClick = {callPhoneNumber()}) {
+fun Greeting() {
+    val context = LocalContext.current
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally){
+        Button(onClick = {callPhoneNumber(context)}) {
             Text("Позвонить")
         }
-        Button(onClick = {}) {
+        Button(onClick = {sendEmail(context, address = "contact@example.com", subject = "Обращение")}) {
             Text("Написать email")
         }
-        Button(onClick = {}) {
+        Button(onClick = {showOnMap(context, latitube = 60.0237, longitude = 30.2289, label = "Наш офис")}) {
             Text("Показать офис на карте")
         }
-        Button(onClick = {}) {
+        Button(onClick = {sharePhone(context, text = "Контакт: +7(495) 123-45-67, contact@example.com")}) {
             Text("Поделиться контактом")
         }
     }
 }
-fun callPhoneNumber(){
-    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+7912347"))
+
+fun callPhoneNumber(context: Context){
+
+    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+74951234567"))
+    if (intent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(intent)}
 }
 
+fun sendEmail(context: Context,address: String, subject: String){
+    val intent = Intent(Intent.ACTION_SENDTO).apply {
+        data = Uri.parse("mailto: ")
+        putExtra(Intent.EXTRA_EMAIL, arrayOf(address))
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+    }
+    if (intent.resolveActivity(context.packageManager) != null){
+        context.startActivity(intent)
+    }
+}
+
+fun showOnMap(context : Context, latitube: Double, longitude: Double, label: String){
+    val geoUri = Uri.parse("geo:0,07?q=$latitube,$longitude($label)")
+    val intent = Intent(Intent.ACTION_VIEW, geoUri)
+    if (intent.resolveActivity(context.packageManager) != null){
+        context.startActivity(intent)
+    }
+}
+
+fun sharePhone(context: Context, text: String){
+    val sendIntent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_TEXT, text)
+    }
+    val chooser = Intent.createChooser(sendIntent, "Поделиться через ...")
+    context.startActivity(chooser)
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Intent_vazingeTheme {
-        Greeting("Android")
-    }
+fun BirthdayCardPreview() {
+    Greeting()
 }
